f4750c1f5eba8e03bea8887ec155ec9c41dc81c1
Henning Jacobs <henning@jacobs1.de>
2015-03-19 19:50:17 +0100

    use clickclick library

--- mai/cli.py
+++ mai/cli.py
@@ -2,22 +2,41 @@ import click
 import keyring
 
 from aws_saml_login import authenticate, assume_role, write_aws_credentials
+from clickclick import Action, choice, error
 
 
 @click.command()
 @click.option('--url')
 @click.option('-U', '--user', envvar='SAML_USER')
 def cli(url, user):
+    if not url:
+        raise click.UsageError('Missing identity provider URL')
+
+    if not user:
+        raise click.UsageError('Missing SAML username')
+
     saml_password = keyring.get_password('mai', user)
     if not saml_password:
         saml_password = click.prompt('Please enter your SAML password', hide_input=True)
 
-    saml_xml, roles = authenticate(url, user, saml_password)
+    with Action('Authenticating against {url}..', url=url):
+        saml_xml, roles = authenticate(url, user, saml_password)
+
     keyring.set_password('mai', user, saml_password)
 
-    role = roles[0]
-    key_id, secret, session_token = assume_role(saml_xml, role[0], role[1])
-    write_aws_credentials('default', key_id, secret, session_token)
+    if not roles:
+        error('No roles found')
+
+    if len(roles) == 1:
+        role = roles[0]
+    else:
+        role = choice('Please select one role', [(r, str(r)) for r in sorted(roles)])
+
+    with Action('Assuming role {role}..', role=role):
+        key_id, secret, session_token = assume_role(saml_xml, role[0], role[1])
+
+    with Action('Writing temporary AWS credentials..'):
+        write_aws_credentials('default', key_id, secret, session_token)
 
 
 def main():
