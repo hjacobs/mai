0c30d41072f5adeee87d17c549651305c0a56c32
Henning Jacobs <henning@jacobs1.de>
2015-04-16 16:56:28 +0200

    add --version flag to print current Mai version

--- mai/cli.py
+++ mai/cli.py
@@ -4,6 +4,8 @@ import keyring
 import yaml
 import aws_saml_login.saml
 
+import mai
+
 from aws_saml_login import authenticate, assume_role, write_aws_credentials
 from clickclick import Action, choice, error, AliasedGroup, info, print_table
 
@@ -13,9 +15,18 @@ CONFIG_FILE_PATH = os.path.join(CONFIG_DIR_PATH, 'mai.yaml')
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo('Mai {}'.format(mai.__version__))
+    ctx.exit()
+
+
 @click.group(cls=AliasedGroup, invoke_without_command=True, context_settings=CONTEXT_SETTINGS)
 @click.option('--config-file', '-c', help='Use alternative configuration file',
               default=CONFIG_FILE_PATH, metavar='PATH')
+@click.option('-V', '--version', is_flag=True, callback=print_version, expose_value=False, is_eager=True,
+              help='Print the current version number and exit.')
 @click.pass_context
 def cli(ctx, config_file):
     path = os.path.expanduser(config_file)
